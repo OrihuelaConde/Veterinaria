6c51991cdd6fc912978f13afd245093c4e63be2f
José Luis Orihuela Conde <orihuelaconde@gmail.com>
2022-06-28 16:07:07 -0300

    Agregar archivos de proyecto.

---
+++ Mascota.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Veterinaria
+{
+    internal class Mascota
+    {
+        public string nombre;
+        /// <summary>
+        /// Perro, gato...
+        /// </summary>
+        public string tipo;
+        public int anioNacimiento;
+        public int mesVencimientoVacunas;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Veterinaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion = 0;
+            List<Mascota> mascotas = new List<Mascota>();
+            do
+            {
+                Console.WriteLine("1- Agregar mascota");
+                Console.WriteLine("2- Mostrar mascotas");
+                Console.WriteLine("0- Salir");
+                Console.Write("\nSeleccione una opción: ");
+                opcion = int.Parse(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1:
+                        Mascota nuevaMascota = new Mascota();
+                        Console.Write("Nombre: ");
+                        nuevaMascota.nombre = Console.ReadLine();
+                        Console.Write("Tipo: ");
+                        nuevaMascota.tipo = Console.ReadLine();
+                        Console.Write("Año de nacimiento: ");
+                        nuevaMascota.anioNacimiento = int.Parse(Console.ReadLine());
+                        Console.Write("Mes de vencimiento de las vacunas: ");
+                        nuevaMascota.mesVencimientoVacunas = int.Parse(Console.ReadLine());
+                        mascotas.Add(nuevaMascota);
+                        break;
+                    case 2:
+                        for(int i = 0; i < mascotas.Count; i++)
+                        {
+                            Console.WriteLine("Nombre: " + mascotas[i].nombre);
+                            Console.WriteLine("Tipo: " + mascotas[i].tipo);
+                            Console.WriteLine("Año de nacimiento: " + mascotas[i].anioNacimiento);
+                            Console.WriteLine("Mes de vencimiento de las vacunas: " + mascotas[i].mesVencimientoVacunas);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            } while (opcion != 0);
+        }
+    }
+}
